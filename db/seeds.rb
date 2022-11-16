4b08ebcf806480718ed941dd17293cdab77030b4
georges ryssen <georges.ryssen@gmail.com>
2022-11-16 14:34:16 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,21 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+10.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_with_country_code,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  puts 'new restaurant created'
+end
+puts 'Finished!'
